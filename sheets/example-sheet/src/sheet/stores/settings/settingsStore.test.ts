4a96ac0fd3cfcb9c3240a5d33023852504977977
matt-quests-for-tacos <cm.williamsberg@gmail.com>
2024-02-26 17:07:24 -0500

    Finish writing test for settings store

--- sheets/example-sheet/src/sheet/stores/settings/settingsStore.test.ts
+++ sheets/example-sheet/src/sheet/stores/settings/settingsStore.test.ts
@@ -3,9 +3,17 @@ import { createPinia } from "pinia";
 import { describe, it, expect } from 'vitest';
 
 describe('settingsStore', () => {
+  const defaultEncumberancePenalty = -4;
+
   it('defaults correct values', () => {
     const pinia = createPinia();
     const store = useSettingsStore(pinia);
     expect(store).toBeTruthy();
+
+    expect(store.dehydrate()).toEqual({
+      settings: {
+        encumbrancePenalty: defaultEncumberancePenalty,
+      },
+    })
   })
 })
