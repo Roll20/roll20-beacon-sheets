e391e0e40ff433c9d191fff41e18871fee7d7dad
nighthunter226-but-real <289420126+nighthunter226-but-real@users.noreply.github.com>
2026-06-06 17:18:29 +1000

    Fix inventory modal template structure

--- ageofficial/src/components/inventory/InventoryModal.vue
+++ ageofficial/src/components/inventory/InventoryModal.vue
@@ -59,11 +59,11 @@
     </div>
   </div>
 </div>
-</div>
+
 
 <div class="row" style="margin:0">
   <div class="mb-3 col">
-    <span class="age-input-label" id="basic-addon1">Item Qualitie </span>
+    <span class="age-input-label" id="basic-addon1">Item Qualities</span>
     <input
       type="text"
       class="form-control"
